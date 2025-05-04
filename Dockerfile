97ac68537d205ff806569bc59a88e2544deee1ef
evertoncsteixeira <evertoncristiano86@uol.com.br>
2025-05-03 23:25:57 -0300

    novo commit

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN mvn clean install
 
 FROM openjdk:21-jdk-slim
 
-EXPOSE 8080
+EXPOSE 8081
 
 COPY --from=build /app/target/sampaio-0.0.1-SNAPSHOT.jar app.jar
 
--- src/main/java/br/com/sampaio/SampaioApplication.java
+++ src/main/java/br/com/sampaio/SampaioApplication.java
@@ -1,13 +1,15 @@
 package br.com.sampaio;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SampaioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampaioApplication.class, args);
+		  new SpringApplicationBuilder(SampaioApplication.class)
+            .properties("server.port=8081")
+            .run(args);
 	}
 
 }
